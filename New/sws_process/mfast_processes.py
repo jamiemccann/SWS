c216b9c5f92a84a1fad75c74fd8d927416ac0a53
Jamie McCann <104585251+jamiemccann@users.noreply.github.com>
2022-07-05 15:35:00 +0100

    Create mfast_processes.py

---
+++ New/sws_process/mfast_processes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import os
+import glob
+
+def run_all_stations(dir_path):
+    """
+    Fucntion which run do_station_mfm in on all stations directories within a specified directory.
+    This function will only work if the directories are in the required structure. This structure is
+    Station > Event-station component files
+
+    dir_path: str, path to the top level directory in which the station directories are contained
+    """
+    for station in sorted(glob.glob(f"{dir_path}/*")):
+        os.system(f"/raid2/jam247/mfast/sample_data/do_station_mfm {station}")
