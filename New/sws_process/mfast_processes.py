b2360bfe31085a354a5f29ef62e8f66ed96e3d6f
Jamie McCann <104585251+jamiemccann@users.noreply.github.com>
2022-07-06 15:26:03 +0100

    Update mfast_processes.py

--- New/sws_process/mfast_processes.py
+++ New/sws_process/mfast_processes.py
@@ -9,7 +9,7 @@ def run_all_stations(dir_path):
     This function will only work if the directories are in the required structure. This structure is
     Station > Event-station component files
 
-    dir_path: str, path to the top level directory in which the station directories are contained
+    dir_path: str, full path to the top level directory in which the station directories are contained
     """
     for station in sorted(glob.glob(f"{dir_path}/*")):
         os.system(f"/raid2/jam247/mfast/sample_data/do_station_mfm {station}")
